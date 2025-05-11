10fe620c6721d95596a1f8b6c25773f3a522e6e8
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-11 14:19:48 +0300

    Add initialization, append, and print functionalities to LinkedList

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -5,12 +5,17 @@ public class LinkedList {
     private Node tail;
     private int length;
 
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
     public LinkedList(int value) {
         Node newNode = new Node(value);
         this.head = newNode;
         this.tail = newNode;
         this.length = 1;
-
     }
 
     public class Node {
@@ -20,5 +25,26 @@ public class LinkedList {
         public Node(int value) {
             this.value = value;
         }
+    }
+
+    public static void printList(LinkedList lk){
+        Node temp = lk.head;
+
+        while (temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (this.length == 0){
+            this.head = newNode;
+            this.tail = newNode;
+        }else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
     }
 }
--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -3,8 +3,11 @@ package datastructures.linkedlist;
 public class Main {
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(5);
+        LinkedList myLinkedList = new LinkedList();
+        myLinkedList.append(5);
         myLinkedList.append(2);
         myLinkedList.append(9);
+
+        LinkedList.printList(myLinkedList);
     }
 }
