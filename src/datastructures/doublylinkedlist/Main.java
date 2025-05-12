be8cf2920da0e25ccffba40d0613a34f3afe1db1
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-12 19:57:32 +0300

    Add removeLast method to DoublyLinkedList

--- src/datastructures/doublylinkedlist/Main.java
+++ src/datastructures/doublylinkedlist/Main.java
@@ -5,9 +5,15 @@ public class Main {
         DoublyLinkedList myDLL = new DoublyLinkedList(3);
         myDLL.append(17);
         myDLL.append(7);
+        myDLL.append(14);
+
         myDLL.getHead();
         myDLL.getTail();
         myDLL.getLength();
         myDLL.printList();
+
+        System.out.println("Removed: " + myDLL.removeLast().value);
+        myDLL.printList();
+
     }
 }
