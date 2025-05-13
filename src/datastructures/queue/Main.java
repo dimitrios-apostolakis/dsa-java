c0a4f22b16816d92533e07edd7637495574093f0
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-13 10:53:07 +0300

    Add enqueue method to Queue implementation

--- src/datastructures/queue/Main.java
+++ src/datastructures/queue/Main.java
@@ -3,6 +3,9 @@ package datastructures.queue;
 public class Main {
     public static void main(String[] args) {
         Queue myQueue = new Queue(5);
+        myQueue.enqueue(12);
+        myQueue.enqueue(13);
+
         myQueue.getFirst();
         myQueue.getLast();
         myQueue.getLength();
--- src/datastructures/queue/Queue.java
+++ src/datastructures/queue/Queue.java
@@ -41,4 +41,16 @@ public class Queue {
             temp = temp.next;
         }
     }
+
+    public void enqueue(int value){
+        Node newNode = new Node(value);
+        if (length == 0){
+            first = newNode;
+            last = newNode;
+        }else{
+            last.next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
 }
