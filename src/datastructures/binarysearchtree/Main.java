c4e47b38bf145f0c432a59f5de4c10bee01847ae
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-13 16:41:16 +0300

    Add insert and contains methods to BinarySearchTree

--- src/datastructures/binarysearchtree/Main.java
+++ src/datastructures/binarysearchtree/Main.java
@@ -7,5 +7,18 @@ public class Main {
 
         System.out.println("Root: " + myBST.root);
 
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+
+        System.out.println(myBST.root.left.right.value);
+
+        System.out.println(myBST.contains(1));
+        System.out.println(myBST.contains(27));
     }
 }
