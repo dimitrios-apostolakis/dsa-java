8601d3214631f8fd807951c46affb1cfaab077ea
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-13 10:41:05 +0300

    Add initial Queue implementation with basic methods

---
+++ src/datastructures/queue/Main.java
@@ -0,0 +1,11 @@
+package datastructures.queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue myQueue = new Queue(5);
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+        myQueue.printQueue();
+    }
+}
