bd57151ef4315c571dfaad929f969f82cdc60b4b
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-14 08:58:33 +0300

    Add basic Graph implementation with vertices and edges

---
+++ src/datastructures/graph/Main.java
@@ -0,0 +1,19 @@
+package datastructures.graph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph myGraph = new Graph();
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.printGraph();
+
+        System.out.println(myGraph.addEdge("A", "B"));
+        System.out.println(myGraph.addEdge("A", "D"));
+        System.out.println(myGraph.addEdge("M", "D"));
+        myGraph.printGraph();
+
+        System.out.println(myGraph.addEdge("C", "B"));
+        myGraph.printGraph();
+    }
+}
