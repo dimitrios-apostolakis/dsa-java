99de57eaf9d51a060e346feabfab2fd66500aab3
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-14 08:08:55 +0300

    Add itemInCommon method to find common elements in arrays

--- src/datastructures/hashtable/Main.java
+++ src/datastructures/hashtable/Main.java
@@ -1,6 +1,8 @@
 package datastructures.hashtable;
 
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -20,5 +22,25 @@ public class Main {
         System.out.println("Get: " + myHT.get("lumber"));
 
         System.out.println("All keys: " + myHT.keys());
+
+        //ex1
+        int[] array1 = {1,3,5};
+        int[] array2 = {2,4,5};
+        int[] array3 = {2,4,6};
+        System.out.println(itemInCommon(array1, array2));
+        System.out.println(itemInCommon(array1, array3));
+    }
+
+    public static boolean itemInCommon(int[] array1, int[] array2){
+        HashMap<Integer, Boolean> myHM = new HashMap<>();
+
+        for (int i : array1){
+            myHM.put(i, true);
+        }
+
+        for (int j : array2){
+            if (myHM.get(j) != null) return true;
+        }
+        return false;
     }
 }
