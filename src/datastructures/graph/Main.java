088354452a789279318d54f03f2a066b08d8835d
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-14 12:43:24 +0300

    Add removeVertex method to Graph with test cases in Main

--- src/datastructures/graph/Main.java
+++ src/datastructures/graph/Main.java
@@ -6,20 +6,27 @@ public class Main {
         myGraph.addVertex("A");
         myGraph.addVertex("B");
         myGraph.addVertex("C");
+        myGraph.addVertex("D");
         myGraph.printGraph();
 
         System.out.println(myGraph.addEdge("A", "B"));
-        System.out.println(myGraph.addEdge("A", "D"));
-        System.out.println(myGraph.addEdge("M", "D"));
+        System.out.println(myGraph.addEdge("A", "L"));
+        System.out.println(myGraph.addEdge("M", "K"));
         myGraph.printGraph();
 
         System.out.println(myGraph.addEdge("C", "B"));
+        System.out.println(myGraph.addEdge("D", "A"));
+        System.out.println(myGraph.addEdge("D", "B"));
+        System.out.println(myGraph.addEdge("D", "C"));
         myGraph.printGraph();
 
         System.out.println(myGraph.removeEdge("A", "B"));
         myGraph.printGraph();
 
         System.out.println(myGraph.removeEdge("A", "C"));
+        myGraph.printGraph();
+
+        System.out.println(myGraph.removeVertex("D"));
         myGraph.printGraph();
     }
 }
