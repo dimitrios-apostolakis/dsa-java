732730febd7d1a901819deace311af8eaab43657
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-16 09:07:08 +0300

    Add hash map solution for validating anagrams

--- src/datastructures/problems/ValidAnagram.java
+++ src/datastructures/problems/ValidAnagram.java
@@ -1,11 +1,12 @@
 package datastructures.problems;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ValidAnagram {
 
     //sorting: time comp O(nlogn + mlogm) | space comp O(1) or O(n+m)
-    public static boolean isAnagram(String s, String t){
+    public static boolean isAnagramS(String s, String t){
         if (s.length() != t.length()) return false;
 
         char[] s1 = s.toCharArray();
@@ -17,9 +18,26 @@ public class ValidAnagram {
         return Arrays.equals(s1,t1);
     }
 
+    //hash map: time comp O(n + m) | space comp O(1) because 26 chars
+    public static boolean isAnagramHM(String s, String t){
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            countS.put(s.charAt(i),countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i),countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+
+        return countS.equals(countT);
+    }
+
     public static void main(String[] args) {
         String s = "racecar";
         String t = "carrace";
-        System.out.println(isAnagram(s,t));
+        System.out.println("Sorting: " + isAnagramS(s,t));
+        System.out.println("Hash Map: " + isAnagramHM(s,t));
     }
 }
