d63d59e0dd6b74c5c5c7a163ea1537f09d716ee4
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-16 11:11:56 +0300

    Refactor method and add hash table solution for GroupAnagrams.

--- src/datastructures/problems/arraysandhashing/GroupAnagrams.java
+++ src/datastructures/problems/arraysandhashing/GroupAnagrams.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class GroupAnagrams {
 
     //sorting: time comp O(m*nlogn) | space comp O(m*n)
-    public static List<List<String>> groupAnagrams(String[] strs){
+    public static List<List<String>> groupAnagramsS(String[] strs){
 
         Map<String, List<String>> res = new HashMap<>();
         for (String s : strs){
@@ -18,10 +18,27 @@ public class GroupAnagrams {
         return new ArrayList<>(res.values());
     }
 
+    //hash table: time comp O(m*n) | space comp O(m*n)
+    public static List<List<String>> groupAnagramsHT(String[] strs){
+
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs){
+            int[] count = new int[26];
+            for(char c : s.toCharArray()){
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+
     public static void main(String[] args) {
 
         String[] strs = {"act","pots","tops","cat","stop","hat"};
 
-        System.out.println("Sorting: " + groupAnagrams(strs));
+        System.out.println("Sorting: " + groupAnagramsS(strs));
+        System.out.println("Hash Table: " + groupAnagramsHT(strs));
     }
 }
