d365b54e8a44ffd0e67ae4d585f7304036b53c83
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-17 21:48:33 +0300

    Add decode method for string decoding functionality

--- src/datastructures/problems/arraysandhashing/EncodeDecode.java
+++ src/datastructures/problems/arraysandhashing/EncodeDecode.java
@@ -16,9 +16,29 @@ public class EncodeDecode {
         return  sb.toString();
     }
 
+    public static List<String> decode(String str) {
+        if (str == null) return null;
+        List<String> res = new ArrayList<>();
+        int i=0;
+        while (i<str.length()){
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+            int length = Integer.parseInt(str.substring(i, j));
+            i = j + 1;
+            j = i + length;
+            res.add(str.substring(i, j));
+            i = j;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         List<String> strs = Arrays.asList("leet", "code", "love", "you");
         String encodedStr = encode(strs);
         System.out.println(encodedStr);
+        List<String> decodedStr = decode(encodedStr);
+        System.out.println(decodedStr);
     }
 }
