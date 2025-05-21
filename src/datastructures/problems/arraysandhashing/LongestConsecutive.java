a15aba30cb5b6f7a758047bfd34e0fbfc3270e32
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-21 10:42:16 +0300

    Refactor and add new implementation for LongestConsecutive

--- src/datastructures/problems/arraysandhashing/LongestConsecutive.java
+++ src/datastructures/problems/arraysandhashing/LongestConsecutive.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class LongestConsecutive {
 
     //brute force: time comp O(n^2) | space comp O(n)
-    public static int longestConsecutive(int[] nums){
+    public static int longestConsecutiveBF(int[] nums){
         int res = 0;
         Set<Integer> store = new HashSet<>();
         for (int num : nums) {
@@ -25,14 +25,41 @@ public class LongestConsecutive {
         return res;
     }
 
+    //sorting: time comp O(nlogn) | space comp O(1) O(n)
+    public static int longestConsecutiveS(int[] nums){
+        if (nums.length == 0) return 0;
+        Arrays.sort(nums);
+        int res = 0, curr = nums[0], streak = 0, i = 0;
+        while (i<nums.length){
+            if (curr != nums[i]){
+                curr = nums[i];
+                streak = 0;
+            }
+
+            while (i<nums.length && nums[i] == curr){
+                i++;
+            }
+            streak++;
+            curr++;
+            res = Math.max(res, streak);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] nums = {2,20,4,10,3,4,5};
         int[] nums2 = {0,3,2,5,4,6,1,1};
         int[] nums3 = {5,0,6,0,8};
 
         System.out.println("Brute force:");
-        System.out.println(longestConsecutive(nums));
-        System.out.println(longestConsecutive(nums2));
-        System.out.println(longestConsecutive(nums3));
+        System.out.println(longestConsecutiveBF(nums));
+        System.out.println(longestConsecutiveBF(nums2));
+        System.out.println(longestConsecutiveBF(nums3));
+
+        System.out.println("\nSorting:");
+        System.out.println(longestConsecutiveS(nums));
+        System.out.println(longestConsecutiveS(nums2));
+        System.out.println(longestConsecutiveS(nums3));
     }
 }
